77208df1be598e4febc4030e218401dbe294b178
tongyipush <tongyipush777888@126.com>
2021-05-30 08:26:09 +0800

    增加打印输出

---
+++ src/main/java/com/bytes/test.java
@@ -0,0 +1,10 @@
+package com.bytes;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("细细钻研");
+        System.out.println("细细钻研");
+        System.out.println("细细钻研");
+        System.out.println("细细钻研");
+    }
+}
